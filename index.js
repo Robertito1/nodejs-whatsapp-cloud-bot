936dcc6e8f71ca9cfe42a1c0e666a7cc9ebdd523
Radi <evaradi18@gmail.com>
2022-05-30 07:55:10 +0100

    today's modifications

--- index.js
+++ index.js
@@ -1,62 +1,62 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express')
+require('dotenv').config();
 
-const axios = require("axios").default;
+const axios = require('axios').default;
 const data = JSON.stringify({
-  messaging_product: "whatsapp",
-  to: "2349013175953",
-  recipient_type: "individual",
-  type: "interactive",
-  interactive: {
-    type: "button",
-    header: { type: "text", text: "Okay ohh" },
-    body: {
-      text: "BUTTON_TEXT",
-    },
-    action: {
-      buttons: [
-        {
-          type: "reply",
-          reply: {
-            id: "UNIQUE_BUTTON_ID_1",
-            title: "TItle",
-          },
+    "messaging_product": "whatsapp",
+    "to": "2349013175953",
+    "recipient_type": "individual",
+    "type": "interactive",
+    "interactive": {
+        "type": "button",
+        "header": {"type": "text", "text": "Okay ohh"},
+        "body": {
+            "text": "BUTTON_TEXT"
         },
-        {
-          type: "reply",
-          reply: {
-            id: "GUuu",
-            title: "Guuy",
-          },
-        },
-      ],
-    },
-  },
-});
+        "action": {
+            "buttons": [
+                {
+                    "type": "reply",
+                    "reply": {
+                        "id": "UNIQUE_BUTTON_ID_1",
+                        "title": "TItle",
+                    }
+                },
+                {
+                    "type": "reply",
+                    "reply": {
+                        "id": "GUuu",
+                        "title": "Guuy"
+                    }
+                }
+            ]
+        }
+    }
+})
 
 const sendMeMessage = () => {
-  axios({
-    method: "post",
-    url: `https://graph.facebook.com/v12.0/${process.env.WA_ID}/messages/`,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.WA_TOKEN}`,
-    },
-    data: data,
-  })
+    axios({
+        method: 'post',
+        url: `https://graph.facebook.com/v12.0/${process.env.WA_ID}/messages/`,
+        headers: { 
+            'Content-Type': 'application/json', 
+            'Authorization': `Bearer ${process.env.WA_TOKEN}`
+        },
+        data: data
+    })
     .then((response) => {
-      console.log(response);
+    console.log(response);
     })
     .catch((error) => {
-      console.log({ token: process.env.WA_TOKEN });
-      console.log(error.response);
-    });
-};
+    console.log({token: process.env.WA_TOKEN})
+    console.log(error.response);
+    })
+}
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("World");
-});
+app.get('/', function (req, res) {
+  res.send('World')
+})
 sendMeMessage();
-app.listen(3000);
+app.listen(3000)
